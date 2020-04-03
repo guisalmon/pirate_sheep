ddc68326ddac268508eb11a8059dfe3e006e1221
Rob <guisalmon@gmail.com>
2020-04-03 14:58:27 +0200

    Implement basic viewmodel

--- app/src/main/java/org/robnetwork/piratesheep/ui/BaseActivity.kt
+++ app/src/main/java/org/robnetwork/piratesheep/ui/BaseActivity.kt
@@ -1,27 +1,49 @@
 package org.robnetwork.piratesheep.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.annotation.CallSuper
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-abstract class BaseActivity<B : ViewDataBinding> : AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding, D : BaseData, VM : BaseViewModel<D>> :
+    AppCompatActivity() {
     @get:LayoutRes
     abstract val layoutRes: Int
+    abstract val viewModelClass: Class<VM>
+
+    protected lateinit var viewModel: VM
 
     protected var binding: B? = null
         private set
 
     final override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<B>(this, layoutRes)?.apply { setupUI(this) } ?: Toast.makeText(this, "MERDE", Toast.LENGTH_SHORT).show()
+        viewModel = ViewModelProvider(this).get(viewModelClass)
+        DataBindingUtil.setContentView<B>(this, layoutRes)?.apply { setupUI(this) }
     }
 
     @CallSuper
     protected open fun setupUI(binding: B) {
         this.binding = binding
+        viewModel.data.observe(
+            this,
+            Observer {
+                updateUI(it)
+            }
+        )
     }
+
+    abstract fun updateUI(data: D)
+}
+
+interface BaseData
+
+abstract class BaseViewModel<D : BaseData> : ViewModel() {
+    abstract val data: MutableLiveData<D>
 }
--- app/src/main/java/org/robnetwork/piratesheep/ui/MainActivity.kt
+++ app/src/main/java/org/robnetwork/piratesheep/ui/MainActivity.kt
@@ -4,27 +4,49 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
 import org.robnetwork.piratesheep.R
 import org.robnetwork.piratesheep.databinding.ActivityMainBinding
 import java.util.*
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainData, MainViewModel>() {
 
     override val layoutRes: Int = R.layout.activity_main
+    override val viewModelClass = MainViewModel::class.java
 
     override fun setupUI(binding: ActivityMainBinding) {
         super.setupUI(binding)
-		val cal = Calendar.getInstance()
-        binding.mainBirthdayEdit.setOnClickListener(showDatePicker(binding.mainBirthdayEdit, cal))
-        binding.mainDateEdit.setDate(
-            cal.get(Calendar.DAY_OF_MONTH),
-            cal.get(Calendar.MONTH) + 1,
-            cal.get(Calendar.YEAR)
+        val cal = Calendar.getInstance()
+
+        binding.mainBirthdayEdit.setOnClickListener(
+            showDatePicker(
+                cal,
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    viewModel.updateBirthday(dateString(dayOfMonth, month + 1, year))
+                })
         )
-        binding.mainDateEdit.setOnClickListener(showDatePicker(binding.mainDateEdit, cal))
-        binding.mainTimeEdit.setTime(cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE))
-        binding.mainTimeEdit.setOnClickListener(showTimePicker(binding.mainTimeEdit, cal))
+        viewModel.updateDate(
+            dateString(
+                cal.get(Calendar.DAY_OF_MONTH),
+                cal.get(Calendar.MONTH) + 1,
+                cal.get(Calendar.YEAR)
+            )
+        )
+        binding.mainDateEdit.setOnClickListener(
+            showDatePicker(
+                cal,
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    viewModel.updateDate(dateString(dayOfMonth, month + 1, year))
+                })
+        )
+        viewModel.updateTime(timeString(cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE)))
+        binding.mainTimeEdit.setOnClickListener(
+            showTimePicker(
+                cal,
+                TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                    viewModel.updateTime(timeString(hourOfDay, minute))
+                })
+        )
         ArrayAdapter(
             this,
             android.R.layout.simple_spinner_item,
@@ -35,37 +57,72 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         }
     }
 
-    private fun showDatePicker(textToUpdate: TextView, cal: Calendar) = View.OnClickListener {
+    override fun updateUI(data: MainData) {
+        binding?.let {
+            it.mainBirthdayEdit.text = data.birthday
+            it.mainDateEdit.text = data.date
+            it.mainTimeEdit.text = data.time
+        }
+    }
+
+    private fun showDatePicker(
+        cal: Calendar,
+        onDateSetListener: DatePickerDialog.OnDateSetListener
+    ) = View.OnClickListener {
         DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener { _, year, m, dayOfMonth ->
-                val month = m + 1
-                textToUpdate.setDate(dayOfMonth, month, year)
-            },
+            onDateSetListener,
             cal.get(Calendar.YEAR),
             cal.get(Calendar.MONTH),
             cal.get(Calendar.DAY_OF_MONTH)
         ).show()
     }
 
-    private fun showTimePicker(textToUpdate: TextView, cal: Calendar) = View.OnClickListener {
+    private fun showTimePicker(
+        cal: Calendar,
+        onTimeSetListener: TimePickerDialog.OnTimeSetListener
+    ) = View.OnClickListener {
         TimePickerDialog(
             this,
-            TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
-                textToUpdate.text =
-                    getString(R.string.time_template, hourOfDay, minute)
-            },
+            onTimeSetListener,
             cal.get(Calendar.HOUR),
             cal.get(Calendar.MINUTE),
             true
         ).show()
     }
 
-    private fun TextView.setDate(day: Int, month: Int, year: Int) {
-        text = getString(R.string.date_template, day, month, year)
+    private fun dateString(day: Int, month: Int, year: Int) =
+        getString(R.string.date_template, day, month, year)
+
+    private fun timeString(hour: Int, minutes: Int) =
+        getString(R.string.time_template, hour, minutes)
+}
+
+data class MainData(
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val birthday: String? = null,
+    val birthPlace: String? = null,
+    val address: String? = null,
+    val city: String? = null,
+    val reason: String? = null,
+    val place: String? = null,
+    val date: String? = null,
+    val time: String? = null
+) : BaseData
+
+class MainViewModel : BaseViewModel<MainData>() {
+    override val data = MutableLiveData(MainData())
+
+    fun updateBirthday(birthday: String) = data.value?.let {
+        data.value = it.copy(birthday = birthday)
     }
 
-    private fun TextView.setTime(hour: Int, minutes: Int) {
-        text = getString(R.string.time_template, hour, minutes)
+    fun updateDate(date: String) = data.value?.let {
+        data.value = it.copy(date = date)
+    }
+
+    fun updateTime(time: String) = data.value?.let {
+        data.value = it.copy(time = time)
     }
 }
